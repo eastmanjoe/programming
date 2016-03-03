d3d0296fc1192d9f326700e8b38a8d0e4b7397f2
Joe Eastman <joe.eastman@drakerenergy.com>
2016-03-03 11:56:22 -0500

    updates to cygwin shell script after setting up Jim P's computer.

--- shell/cygwin_setup.sh
+++ shell/cygwin_setup.sh
@@ -6,46 +6,32 @@
 clear
 
 echo "Script to set-up Cygwin"
-read -e -p "Please enter location of home (~): " home_folder
 
 if [ "$(uname -s)" == "Linux" ]; then
     echo "You are running Linux, script not setup for that."
 
 elif [ "$(expr substr $(uname -s) 1 6)" == "CYGWIN" ]; then
 
-    # mkgroup -d -l > /etc/group
-    # mkpasswd -d -l -p $home_folder > /etc/password
+    if [ -e /etc/group ] && [ -e ]
 
 
+    # download apt-cyg
     lynx -source rawgit.com/transcode-open/apt-cyg/master/apt-cyg > apt-cyg
 
     install apt-cyg /bin
 
+    # install required packages for draker
     apt-cyg install wget
-
-    apt-cyg install bash-completion cygutils git git-completion git-gui \
-            inetutils libsqlite3-devel libxml2-devel libxslt-devel make \
-            mintty nano ncurses openssh ping rxvt screen util-linux
-
+    apt-cyg install bash-completion cygutils git git-completion git-gui
+    apt-cyg install inetutils libsqlite3-devel libxml2-devel libxslt-devel make
+    apt-cyg install mintty nano ncurses openssh ping rxvt util-linux
     apt-cyg install openssl
 
-    #setup python
-    # ln -sf /cygdrive/c/Python27/python.exe /usr/bin/python
-    # ln -s /cygdrive/c/Python27/mkvirtualenv.bat /usr/bin/mkvirtualenv
-    # ln -s /cygdrive/c/Python27/lsvirtualenv.bat /usr/bin/lsvirtualenv
-    # ln -s /cygdrive/c/Python27/rmvirtualenv.bat /usr/bin/rmvirtualenv
-    # ln -s /cygdrive/c/Python27/workon.bat /usr/bin/workon
-    # ln -s /cygdrive/c/Python27/deactivate.bat /usr/bin/deactivate
-    # ln -s /cygdrive/c/Python27/add2virtualenv.bat /usr/bin/add2virtualenv
-
-    # RVM requirements
-    apt-cyg install patch openssl-devel libyaml-devel libyaml0_2 sqlite3 gcc-g++ \
-                    gcc-core mingw64-i686-gcc-g++ mingw64-x86_64-gcc-g++ bison \
-                    automake m4 autoconf libtool cygwin32-readline zlib-devel
-
     # install ruby
-    curl -#L https://get.rvm.io | bash -s stable --autolibs=3 --ruby=1.9.3 --auto-dotfiles
+    apt-cyg install ruby
 
+    # curl -#L https://get.rvm.io | bash -s stable --autolibs=3 --ruby=1.9.3 --auto-dotfiles
+    wget
     # # set PATH to use windows python instead of cygwin python
     # # PYTHON_DIR="/cygdrive/c/Python27"
 
@@ -68,4 +54,16 @@ elif [ "$(expr substr $(uname -s) 1 6)" == "CYGWIN" ]; then
     # if [ -d ${CS_COMPILE_200} ] ; then
     #     PATH=${CS_COMPILE_200}:${PATH}
     # fi
+    else
+        read -e -p "Please enter location of home (default: /home): " home_folder
+
+        if [ -z "$home_folder"]; then
+            home_folder = "/home"
+        fi
+
+        mkgroup -d -l > /etc/group
+        mkpasswd -d -l -p $home_folder > /etc/password
+
+        exit 0
+    fi
 fi
