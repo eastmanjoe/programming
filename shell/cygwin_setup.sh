74e8f1c5db40793a40463199ae58ed3f32e1f548
Joe Eastman <joe.eastman@drakerenergy.com>
2016-03-31 10:07:05 -0400

    replace apt-cyg with sage, reformated the file to use functions and provide help if the user does not specify an option

--- shell/cygwin_setup.sh
+++ shell/cygwin_setup.sh
@@ -1,52 +1,85 @@
 #!/bin/bash
 #
-# create copy of image
 #
-
-clear
 
 echo "Script to set-up Cygwin"
 
-if [ "$(uname -s)" == "Linux" ]; then
-    echo "You are running Linux, script not setup for that."
+#Help info
+help()
+{
+echo "Please enter an option"
+echo "-i or --initial_install to setup cygwin for the domain and set the home drive"
+echo "-c or --continue to install the required packages"
+}
 
-elif [ "$(expr substr $(uname -s) 1 6)" == "CYGWIN" ]; then
+# setup home location
+set_home()
+{
+    read -e -p "Please enter location of home (default: /home): " home_folder
 
-    if [ -e /etc/group ] && [ -e ]
+    if [ -z "$home_folder"]; then
+        home_folder = "/home"
+    fi
 
+    mkgroup -d -l > /etc/group
+    mkpasswd -d -l -p $home_folder > /etc/password
 
-    # download apt-cyg
-    lynx -source rawgit.com/transcode-open/apt-cyg/master/apt-cyg > apt-cyg
+    echo "Home folder set and cygwin setup for domain"
+    echo "Exiting Script. Please restart Cygwin"
+    echo "Rerun the setup script with the following command to continue"
+    echo "cygwin_setup.sh -c"
 
-    install apt-cyg /bin
+    exit 0
+}
+
+# verify OS is Cygwin
+check_os()
+{
+    if [ "$(uname -s)" == "Linux" ]; then
+        echo "You are running Linux, script not setup for that. Exiting script"
+        exit 0
+
+    elif [ "$(expr substr $(uname -s) 1 6)" == "CYGWIN" ]; then
+        echo "You are running Cygwin, script continuing."
+
+    else
+        echo "OS is unknown, script not setup for that. Exiting script"
+        exit 0
+    fi
+}
+
+# install the packages
+install_packages()
+{
+
+    # download and install sage
+    lynx -source rawgit.com/svnpenn/sage/master/sage > sage
+    install sage /bin
+    rm sage
+
 
     # install required packages for draker
-    apt-cyg install wget
-    apt-cyg install bash-completion cygutils git git-completion git-gui
-    apt-cyg install inetutils libsqlite3-devel libxml2-devel libxslt-devel make
-    apt-cyg install mintty nano ncurses openssh ping rxvt util-linux
-    apt-cyg install openssl
+    sage install wget
+    sage install bash-completion cygutils git git-completion git-gui
+    sage install inetutils libsqlite3-devel libxml2-devel libxslt-devel make
+    sage install mintty nano ncurses openssh ping rxvt util-linux
+    sage install openssl
 
     # install ruby
-    apt-cyg install ruby
+    sage install ruby rubygems
 
-    # curl -#L https://get.rvm.io | bash -s stable --autolibs=3 --ruby=1.9.3 --auto-dotfiles
-    wget
-    # # set PATH to use windows python instead of cygwin python
-    # # PYTHON_DIR="/cygdrive/c/Python27"
+}
 
-    # # if [ -d ${PYTHON_DIR} ] ; then
-    # #     PATH=${PYTHON_DIR}:${PATH}
-    # # fi
-
-    # # source /cygdrive/c/Python27/Scripts/virtualenvwrapper.sh
-
-    # # set PATH so it includes the campbellsci compilers
-    # CS_COMPILE="/cygdrive/c/Campbellsci/Lib/Compilers"
-
-    # if [ -d ${CS_COMPILE} ] ; then
-    #     PATH=${CS_COMPILE}:${PATH}
-    # fi
+# add the CSI compilers to the cygwin PATH
+set_CSI_compiler_path()
+{
+    echo -e "# set PATH so it includes the campbellsci compilers\n" >> .bashrc
+    echo -e "\n" >> .bashrc
+    echo -e 'CS_COMPILE="/cygdrive/c/Campbellsci/Lib/Compilers"\n' >> .bashrc
+    echo -e "if [ -d ${CS_COMPILE} ] ; then\n" >> .bashrc
+    echo -e "    PATH=${CS_COMPILE}:${PATH}\n" >> .bashrc
+    echo -e "fi\n" >> .bashrc
+    echo -e "\n" >> .bashrc
 
     # # set path to include campbell sci 200 compilers
     # CS_COMPILE_200="/cygdrive/c/Campbellsci/Lib/CR200Compilers"
@@ -54,16 +87,35 @@ elif [ "$(expr substr $(uname -s) 1 6)" == "CYGWIN" ]; then
     # if [ -d ${CS_COMPILE_200} ] ; then
     #     PATH=${CS_COMPILE_200}:${PATH}
     # fi
-    else
-        read -e -p "Please enter location of home (default: /home): " home_folder
+}
 
-        if [ -z "$home_folder"]; then
-            home_folder = "/home"
-        fi
 
-        mkgroup -d -l > /etc/group
-        mkpasswd -d -l -p $home_folder > /etc/password
+# main loop
+if [[ $# = 0 ]];
+then
+    help
+else
+    while [[ $# > 0 ]]
+    do
+        key="$1"
 
-        exit 0
-    fi
+        check_os
+
+        case $key in
+            -i|--initial_install)
+            set_home
+            shift # past argument
+            ;;
+            -c|--continue)
+            install_packages
+            shift
+            ;;
+                *)
+            help
+            shift
+            ;;
+        esac
+        shift # past argument or value
+    done
+    source .bashrc
 fi
